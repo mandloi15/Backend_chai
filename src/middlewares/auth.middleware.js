299e52fc2741e57a27a2384b70342b2ee9f599e8
Developer <mandloikratik600@gmail.com>
2026-05-08 00:08:28 +0530

    feat: enhance JWT verification middleware with error handling for unauthorized access

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,23 @@
+const { ApiError } = require('../utils/ApiError.js')
 const { asyncHandler } = require('../utils/asyncHandler.js')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User.js')
 
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
     const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+    if (!user) {
+        throw new ApiError(401, "Invalid Access Token")
+    }
+    req.user = user
+    next()
 })
