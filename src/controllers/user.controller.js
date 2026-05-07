71a79f8b15c6e4a332a1a366fc2ba76d21e21b16
Developer <mandloikratik600@gmail.com>
2026-05-07 18:21:48 +0530

    feat: implement login functionality with email/username validation and password check

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -70,4 +70,32 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
-module.exports = { registerUser }
+const loginUser = asyncHandler(async (req, res) => {
+    // req body -> data
+    //username or email
+    // find the user
+    // password check
+    // access and refresh token
+    // send cookies
+
+    const{email, username, password} = req.body
+
+    if (!username || !email) {
+        throw new ApiError(400, "Username or email is required")
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }]
+    })
+
+    if (!user) {
+        throw new ApiError(404, "User not found")
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password)
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid password")
+    }
+})
+
+module.exports = { registerUser, loginUser }
