519f3d6d4400139354da373c4634e1de5dc1d1b1
mandloi15 <mandloikratik600@gmail.com>
2026-05-08 09:52:33 +0530

    feat: implement logout functionality with token invalidation and cookie clearance

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -144,7 +144,34 @@ const loginUser = asyncHandler(async (req, res) => {
 })
 
 const logoutUser = asyncHandler(async (req, res) => {
-    
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const option = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", option)
+    .clearCookie("refreshToken", option)
+    .json(
+        new ApiResponse(
+            200,
+            {},
+            "User logged out successfully"
+        )
+    )
 })
 
 module.exports = { registerUser, loginUser, logoutUser }
