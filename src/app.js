8868e7f6cdf0592811424697034e8c4c31cad158
mandloi15 <mandloikratik600@gmail.com>
2026-05-10 01:06:49 +0530

    refactor: rename userRoutes to userRouter and organize route declarations

--- src/app.js
+++ src/app.js
@@ -17,10 +17,26 @@ app.use(cookieParser())
 
 // Routes
 
-const userRoutes = require('./routes/user.routes.js')
+const userRouter = require('./routes/user.routes.js')
+const healthcheckRouter = require("./routes/healthcheck.routes.js")
+const tweetRouter = require("./routes/tweet.routes.js")
+const subscriptionRouter = require("./routes/subscription.routes.js")
+const videoRouter = require("./routes/video.routes.js")
+const commentRouter = require("./routes/comment.routes.js")
+const likeRouter = require("./routes/like.routes.js")
+const playlistRouter = require("./routes/playlist.routes.js")
+const dashboardRouter = require("./routes/dashboard.routes.js")
 
 //routes declaration
-app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos", videoRouter)
+app.use("/api/v1/comments", commentRouter)
+app.use("/api/v1/likes", likeRouter)
+app.use("/api/v1/playlists", playlistRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
 
 
 module.exports = app
