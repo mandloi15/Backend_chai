b30768dbe404b815237a13a3a77a04720508fd6a
mandloi15 <mandloikratik600@gmail.com>
2026-05-10 01:08:27 +0530

    fix: correct formatting of commentSchema definition in comment model

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -14,7 +14,10 @@ const commentSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User"
     }
-},{timestamps: true})
+},
+{
+    timestamps: true
+})
 
 commentSchema.plugin(mongooseAggregatePaginate)
 
